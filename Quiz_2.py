337554f75d0372c6a5ca74796827f4260eb6ee26
weijia_cheng1 <weijia_cheng1@baylor.edu>
2021-11-16 21:11:44 -0600

    QUIZ#2

---
+++ Quiz_2.py
@@ -0,0 +1,18 @@
+'''
+These files contain data for video game ratings given out by the ESRB. 
+Both files contain 34 features. 32 of these contribute to the ESRB's assigned rating. 
+The data has already been split into a train and a test file for you to use to train and test your model. 
+Your goal is to create a model that will take the 32 features for each game and predict a rating. 
+Keep in mind that the final output should be the name of the target rating and not the number. 
+Please refer to this file which should match your output.
+
+Using methods covered in class:
+
+1) load the dataset and use the KNeighborsClassifier to train and test your model
+
+2) Display all wrong predicted and expected pairs
+
+3) produce a csv file of the name of the game and the predicted rating
+
+'''
+
